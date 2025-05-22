1f5810f3035e0ec660a8d29152371cf4f6723e6a
benlug <benedikt.lugauer@pm.me>
2025-05-22 22:51:52 +0200

    update

--- testing_zone/test_data/investigate_test_fits.R
+++ testing_zone/test_data/investigate_test_fits.R
@@ -14,11 +14,10 @@ library(ggplot2)
 
 # Determine script directory and base directory for results
 SCRIPT_DIR <- tryCatch(this.path::this.dir(), error = function(e) getwd())
-ROOT_DIR <- normalizePath(file.path(SCRIPT_DIR, ".."))
 
 # --- Configuration ---
-fits_dir <- file.path(ROOT_DIR, "test_fits")
-plots_dir <- file.path(ROOT_DIR, "fit_diagnostics")
+fits_dir <- file.path(SCRIPT_DIR, "test_fits")
+plots_dir <- file.path(SCRIPT_DIR, "fit_diagnostics")
 if (!dir.exists(plots_dir)) dir.create(plots_dir, recursive = TRUE)
 
 fit_files <- list.files(fits_dir, pattern = "\\.rds$", full.names = TRUE)
@@ -34,13 +33,15 @@ make_traceplot <- function(fit, param, file_stub) {
   p <- rstan::traceplot(fit, pars = param, inc_warmup = FALSE) +
     ggtitle(sprintf("Traceplot for %s", param))
   ggsave(file.path(plots_dir, paste0(file_stub, "_", param, "_trace.png")),
-         plot = p, width = 6, height = 4)
+    plot = p, width = 6, height = 4
+  )
 }
 
 # --- Iterate over fits ---
 for (ff in fit_files) {
   fit <- readRDS(ff)
-  summ <- summary(fit)$summary %>% as.data.frame() %>%
+  summ <- summary(fit)$summary %>%
+    as.data.frame() %>%
     tibble::rownames_to_column(var = "parameter") %>%
     dplyr::select(parameter, mean, sd, n_eff = n_eff, Rhat)
   summaries[[basename(ff)]] <- summ
@@ -58,4 +59,3 @@ summary_df <- dplyr::bind_rows(summaries, .id = "fit_file")
 summary_out <- file.path(plots_dir, "fit_summaries.csv")
 readr::write_csv(summary_df, summary_out)
 cat("Saved summary diagnostics to", summary_out, "\n")
-
